edb75d7e0d6356f084f79a514783e44c3e3b89fa
Duynhat989 <vietduy989kc@gmail.com>
2023-08-12 15:16:19 +0700

    Commit message

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 var dbConn = mysql.createConnection({
-    host: 'localhost',
+    host: '103.200.23.98',
     user: 'vinhcity_vietduy989',
     password: 'JHxS0qb{z[%7',
     database: 'vinhcity_base'
